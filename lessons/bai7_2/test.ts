191b7f4bb2d8fab6ba2649ac8a09a0720e2bfa46
Duy Anh <duyanhdao297@gmail.com>
2025-10-17 16:41:49 +0700

    chore(lessons): complete bai7_2

--- lessons/bai7_2/test.ts
+++ lessons/bai7_2/test.ts
@@ -1,2 +1,5 @@
 // Không cần kiểm thử bằng TypeScript
 console.log("Test contract trực tiếp bằng hardhat.");
+// Nộp bài
+// Địa chỉ contract: 0x07cC7dD5e5332b03eaB1346fcE0ae65928BB7722
+// Link Etherscan: https://sepolia.etherscan.io/address/0x07cC7dD5e5332b03eaB1346fcE0ae65928BB7722#code
